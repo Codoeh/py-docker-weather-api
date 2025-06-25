1ea8b89640e6515d3fd3a9c8306837020f8c0245
Mateusz Pawlak <pawlakmateusz96@gmail.com>
2025-06-25 09:23:09 +0200

    solution2

--- app/main.py
+++ app/main.py
@@ -3,13 +3,15 @@ import os
 import requests
 
 
+API_KEY = os.getenv("API_KEY")
+URL = "http://api.weatherapi.com/v1/current.json"
+FILTERING = "Paris"
+
+
 def get_weather() -> None:
-    API_KEY = os.getenv("API_KEY")
     if not API_KEY:
         print("API KEY not found. Set environment variable.")
         return
-    URL = "http://api.weatherapi.com/v1/current.json"
-    FILTERING = "Paris"
     params = {
         "key": API_KEY,
         "q": FILTERING
@@ -19,14 +21,15 @@ def get_weather() -> None:
         response = requests.get(URL, params=params)
         response.raise_for_status()
         data = response.json()
-        location = data["location"]["name"]
+        city = data["location"]["name"]
         country = data["location"]["country"]
         time = data["current"]["last_updated"]
-        temperature = data["current"]["temp_c"]
+        temp = data["current"]["temp_c"]
         condition = data["current"]["condition"]["text"]
-        print(f"{location}/{country} {time} Weather: {temperature} Celsius, {condition}")
+        print(f"{city}/{country} {time} Weather: {temp} Celsius, {condition}")
     except requests.RequestException as e:
         print("Error while getting weather data:", e)
+
 
 if __name__ == "__main__":
     get_weather()
